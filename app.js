6bd2035cfd21079512650cc592960c8889d6e4a9
Daniel Kawakami <s2it_dkawakami@uolinc.com>
2019-12-12 14:33:40 -0300

    Criação do CRUD - Parte 1

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Book = require('./Book.model');
+
+var db = 'mongodb://localhost/example';
+
+mongoose.connect(db, { useNewUrlParser:true,
+                       useUnifiedTopology: true});
+
+var port = 8080;
+
+app.get('/', function(req, res){
+    res.send('happy to be here');
+});
+
+app.get('/books', function(req,res){
+    console.log('getting all books');
+    Book.find({})
+        .exec(function(err, books){
+            if(err) {
+                res.send('error has occured');
+            } else {
+                console.log(books);
+                res.json(books);
+            }
+
+        });
+});
+
+app.listen(port, function() {
+    console.log('app listenin on port ' + port);
+});
